9c59dfa3b6a642cd6768e946c82f7aa19886b40d
uzma1411 <47413610+uzma1411@users.noreply.github.com>
2022-04-18 13:00:56 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,35 @@
+import firebase from 'firebase'
+import 'firebase/auth'
+import 'firebase/database'
+import 'firebase/app'
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyCu6zuQgOSGp6xv4Xro5lVWljV8A1c5ddA",
+  authDomain: "c85-17628.firebaseapp.com",
+  databaseURL: "https://c85-17628-default-rtdb.firebaseio.com",
+  projectId: "c85-17628",
+  storageBucket: "c85-17628.appspot.com",
+  messagingSenderId: "733679622786",
+  appId: "1:733679622786:web:6160199ae7b68056e8729b"
+};
+
+/**apiKey: "AIzaSyCu6zuQgOSGp6xv4Xro5lVWljV8A1c5ddA",
+  authDomain: "c85-17628.firebaseapp.com",
+  databaseURL: "https://c85-17628-default-rtdb.firebaseio.com",
+  projectId: "c85-17628",
+  storageBucket: "c85-17628.appspot.com",
+  messagingSenderId: "733679622786",
+  appId: "1:733679622786:web:6160199ae7b68056e8729b"
+  
+  apiKey: 'AIzaSyDa3_Bv77FRDoW-P95uVI1sISo5JdM69bY',
+  authDomain: 'hotel-chatbot-demo.firebaseapp.com',
+  databaseURL: 'https://hotel-chatbot-demo-default-rtdb.firebaseio.com',
+  projectId: 'hotel-chatbot-demo',
+  storageBucket: 'hotel-chatbot-demo.appspot.com',
+  messagingSenderId: '1092137861522',
+  appId: '1:1092137861522:web:025039ed5ee2809e17814b',
+  measurementId: 'G-V8HVEEE1SY',
+  
+  
+  
+   */
